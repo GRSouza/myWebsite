744ab0efa356f04a3f73d696721e1ceb61a5f0f2
Guilherme Ramos <guilherme.rdss97@gmail.com>
2025-01-30 21:03:55 -0300

    New experience list animation

--- script.js
+++ script.js
@@ -8,16 +8,13 @@ var backToTopButton = document.querySelector(".footer-button");
 
 setInterval(updateClock, 1000);
 updateClock();
-experienceHover();
+experienceHighlight();
 
 function showDropdownMenu() {
     var dropdownMenu = document.querySelector("#dropdown-menu-mask");
     dropdownMenu.classList.toggle("show");
 }
-function showExperienceDescription() {
-    var experienceDescriptionList = document.querySelector(".experience-description-list");
-    experienceDescriptionList.classList.toggle("show");
-}
+
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
@@ -35,10 +32,11 @@ function updateClock() {
         var timeString = formatter.format(now);
         document.getElementById("clock").textContent = timeString;
 }  
-function experienceHover() {
+function experienceHighlight() {
     var expList = document.querySelectorAll(".experience-topic");
     var expBox = document.querySelectorAll(".experience-description-box-content");
-    var expListDesc = document.querySelectorAll(".experience-description-list")
+    var expListDesc = document.querySelectorAll(".experience-description-list");
+    var chevronIcon = document.querySelectorAll(".chevron-icon");
 
     function matchExpNumber() {
         var expListNumber = this.classList[1];
@@ -54,26 +52,36 @@ function experienceHover() {
                 console.log(expBoxNumber + " False");
             }
         }
-
         for (var expListDescClassNr = 0; expListDescClassNr < expListDesc.length; expListDescClassNr++) {
             var expListDescNumber = expListDesc[expListDescClassNr].classList[1];
+            
 
             if (expListNumber === expListDescNumber) {
-                expListDesc[expListDescClassNr].classList.remove("display-off");
+                expListDesc[expListDescClassNr].classList.add("open");
                 console.log(expListDescNumber + " True");
             } else {
-                expListDesc[expListDescClassNr].classList.add("display-off");
+                expListDesc[expListDescClassNr].classList.remove("open");
                 console.log(expListDescNumber + " False");
             }
         }
+        for (var expChevronIconClassNr = 0; expChevronIconClassNr < chevronIcon.length; expChevronIconClassNr++) {
+            var expChevronIconNumber = chevronIcon[expChevronIconClassNr].classList[1];
 
+            if (expListNumber === expChevronIconNumber){
+                chevronIcon[expChevronIconClassNr].classList.add("fa-chevron-up");
+                chevronIcon[expChevronIconClassNr].classList.remove("fa-chevron-down");
+            } else {
+                chevronIcon[expChevronIconClassNr].classList.remove("fa-chevron-up");
+                chevronIcon[expChevronIconClassNr].classList.add("fa-chevron-down");                
+            }
+        }
+        
         expList.forEach(function(topic) {
             topic.classList.remove("topic-highlight");
         })
 
         this.classList.add("topic-highlight");
     }
-
     for (var expListClassNr = 0; expListClassNr < expList.length; expListClassNr++) {
         expList[expListClassNr].addEventListener("click", matchExpNumber);
     }
